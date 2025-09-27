2c910c589308de0546578dd68355269e22375a0c
Dmitry Shilin <dshilin052@gmail.com>
2025-09-27 17:08:51 +0300

    Add Yahoo Finance API call using libcurl
    Introduced a curl_test function that fetches SPY chart data from Yahoo Finance using libcurl and outputs the response. This function is now called in the trading bot's main loop for demonstration or testing purposes.

--- src/trading_bot.cpp
+++ src/trading_bot.cpp
@@ -1,11 +1,36 @@
 #include "trading_bot.h"
 
+#include <curl/curl.h>
+
 #include <csignal>
 #include <format>
 #include <iostream>
 #include <thread>
 
 static std::atomic<bool> g_interrupted{false};
+
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb,
+                            void* userp) {
+  ((std::string*)userp)->append((char*)contents, size * nmemb);
+  return size * nmemb;
+}
+
+void curl_test() {
+  CURL* curl = curl_easy_init();
+  std::string readBuffer;
+
+  if (curl) {
+    curl_easy_setopt(
+        curl, CURLOPT_URL,
+        "https://query1.finance.yahoo.com/v8/finance/chart/SPY?interval=1m");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+
+    std::cout << "Response: " << readBuffer << std::endl;
+  }
+}
 
 static void signal_handler(int) {
   std::cout << "User aborted" << std::endl;
@@ -25,8 +50,9 @@ void trading_bot::start(std::chrono::milliseconds interval) {
   std::signal(SIGINT, signal_handler);
 
   while (is_running.load() && !g_interrupted.load()) {
-    std::cout << trading_bot::get_current_time_and_date() << " -- bot is working"
-              << std::endl;
+    std::cout << trading_bot::get_current_time_and_date()
+              << " -- bot is working" << std::endl;
+    curl_test();
 
     std::this_thread::sleep_for(interval);
   }
